e6195c9c87c87b968f480d72a2dcc4cb63e5ffa4
dilovarbobokulov@gmail.com <dilovarbobokulov@gmail.com>
2021-12-13 23:54:06 +0500

    ptoject new

--- src/Components/index.jsx
+++ src/Components/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Cart1 from '../Cart-1/Cart'
 import Cart2 from '../Cart-2/Cart'
 import Cart3 from '../Cart-3/Cart'
@@ -11,16 +11,12 @@ const Components = () => {
             <BrowserRouter>
                 <Routes>
                     <Route exact path="/" element={<Cart1 />} />
-                </Routes>
-                <Routes>
                     <Route path="/sign-in" element={<Cart2 />} />
-                </Routes>
-                <Routes>
                     <Route path="/sign-up" element={<Cart3 />} />
-                </Routes>
-                <Routes>
                     <Route path="/welcome" element={<Cart4 />} />
+                    <Route path="*" element={<Link to="/">home</Link>} />
                 </Routes>
+
             </BrowserRouter>
         </div>
     )
